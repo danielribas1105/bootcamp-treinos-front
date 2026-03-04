17479eae443881f1fae45d7a84d453c2f0eb4876
daniel.ribas <daniel.ribas@isdbflowtech.com>
2026-03-04 15:30:09 -0300

    feat: add shadcn components and others

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { globals: globals.browser } 
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		languageOptions: { globals: globals.browser },
+	},
+	tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	eslintConfigPrettier,
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	},
+])
